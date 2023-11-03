5341d8065abe12174fb9cbc317439d215f58f564
yhc010403 <79885846+yhc010403@users.noreply.github.com>
2023-11-03 15:13:57 +1100

    Update apriltag_detector.cpp

--- src/apriltag_detector.cpp
+++ src/apriltag_detector.cpp
@@ -1,96 +1,6 @@
 //--Includes-----------------------------------------------------------
 #include "hestia/apriltag_detector.h"
-
-// #include <ros/ros.h>
-// #include <sensor_msgs/CompressedImage.h>
 #include <std_msgs/Int32MultiArray.h>
-// #include <opencv2/opencv.hpp>
-// #include <opencv2/highgui/highgui.hpp>
-// #include <apriltag/apriltag.h>
-// #include <apriltag/tag36h11.h>
-// #include <apriltag/tag25h9.h>
-// #include <apriltag/apriltag_pose.h>
-// #include <apriltag/tagStandard41h12.h>
-// #include <opencv2/imgproc/imgproc.hpp>
-// #include <cv_bridge/cv_bridge.h>
-
-// class AprilTagDetector {
-// private:
-//     ros::NodeHandle nh;
-//     ros::Subscriber imageSub;
-//     ros::Publisher tags_pub_;
-//     apriltag_family_t *tf;
-//     apriltag_detector_t *td;
-
-// public:
-// AprilTagDetector::AprilTagDetector() : nh("~") {
-//         ROS_INFO("Node started.");
-//         imageSub = nh.subscribe("/camera/image_raw/compressed", 1, &AprilTagDetector::imageCallback, this);
-//         tags_pub_ = nh.advertise<std_msgs::Int32MultiArray>("/tag_detections", 1);
-
-//         // Initialize AprilTag stuff
-//         tf = tag36h11_create();
-//         td = apriltag_detector_create();
-//         apriltag_detector_add_family(td, tf);
-//         td->quad_decimate = 1.0;
-//         td->quad_sigma = 0.0;
-//     }
-
-// AprilTagDetector::~AprilTagDetector() {
-//         apriltag_detector_destroy(td);
-//         tag36h11_destroy(tf);
-//     }
-
-// void AprilTagDetector::imageCallback(const sensor_msgs::CompressedImageConstPtr& msg) {
-//     ROS_INFO("Received image callback");
-//         // Convert compressed ROS image to OpenCV format
-//         cv::Mat compressedImage = cv::imdecode(cv::Mat(msg->data), 1);
-//         cv::Mat grayImage;
-//         cv::cvtColor(compressedImage, grayImage, CV_BGR2GRAY);
-//         image_u8_t im = {.width = grayImage.cols, .height = grayImage.rows, .stride = grayImage.cols, .buf = grayImage.data};
-
-//         // Detect AprilTags
-//         zarray_t *detections = apriltag_detector_detect(td, &im);
-//     ROS_INFO("Detected tags count: %d", zarray_size(detections));
-
-//         // Extract IDs and publish
-//         std_msgs::Int32MultiArray tag_ids;
-//         for (int i = 0; i < zarray_size(detections); i++) {
-//             apriltag_detection_t *det;
-//             zarray_get(detections, i, &det);
-//             tag_ids.data.push_back(det->id);
-//         }
-
-//         tags_pub_.publish(tag_ids);
-//     ROS_INFO("Published tag detections");
-//         apriltag_detections_destroy(detections);
-//     }
-// };
-
-
-
-// #include <ros/ros.h>
-// #include <sensor_msgs/CompressedImage.h>
-// #include <std_msgs/Int32.h>
-// #include <opencv2/opencv.hpp>
-// #include <opencv2/highgui/highgui.hpp>
-// #include <apriltag/apriltag.h>
-// #include <apriltag/tag36h11.h>
-// #include <apriltag/tag25h9.h>
-// #include <apriltag/apriltag_pose.h>
-// #include <apriltag/tagStandard41h12.h>
-// #include <opencv2/imgproc/imgproc.hpp>
-// #include <cv_bridge/cv_bridge.h>
-
-// class AprilTagDetector {
-// private:
-//     ros::NodeHandle nh;
-//     ros::Subscriber imageSub;
-//     ros::Publisher tagPub;
-//     apriltag_family_t* tf;
-//     apriltag_detector_t* td;
-
-// public:
 
 //--AprilTagDetector Implementation---------------------------------------------------
 AprilTagDetector::AprilTagDetector() : nh("~") 
@@ -114,6 +24,7 @@ AprilTagDetector::~AprilTagDetector()
     tag36h11_destroy(tf);
 }
 
+// Calculate tag size according to pixel position of four corners.
 double computeTagSize(const apriltag_detection_t* det) 
 {
     double x0 = det->p[0][0], y0 = det->p[0][1];
@@ -170,11 +81,4 @@ void AprilTagDetector::imageCallback(const sensor_msgs::CompressedImageConstPtr&
 
     apriltag_detections_destroy(detections);
 }
-// };
 
-// int main(int argc, char** argv) {
-//     ros::init(argc, argv, "apriltag_detector");
-//     AprilTagDetector detector;
-//     ros::spin();
-//     return 0;
-// }
